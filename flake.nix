632fc38fd2047b99f9ab265b3804d821fcbdc8a5
Jademalo <386846+Jademalo@users.noreply.github.com>
2026-09-05 18:04:06 +0000

    Simplify imports and actually import package for flake

--- flake.nix
+++ flake.nix
@@ -5,30 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-
-      overlay = final: prev: {
-        webzfs = final.callPackage ./ports/nix/package.nix { src = self; };
-      };
-    in
+  outputs = 
+    { 
+      self, 
+      nixpkgs 
+    }:
     {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-        in
-        {
-          webzfs = pkgs.webzfs;
-          default = pkgs.webzfs;
-        }
-      );
 
       nixosModules = {
         webzfs = import ./ports/nix/module.nix;
+        default = { ... }: {
+          imports = [ webzfs ];
+        };
       };
 
-      overlays.default = overlay;
     };
 }
