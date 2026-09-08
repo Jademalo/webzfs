239d6985fb6a264434070e7de0037165ba800006
Jademalo <386846+Jademalo@users.noreply.github.com>
2026-09-08 09:36:25 +0000

    Try again to fix sudo

--- ports/nix/package.nix
+++ ports/nix/package.nix
@@ -120,7 +120,6 @@ buildNpmPackage {
       --add-flags "-c $out/opt/webzfs/config/gunicorn.conf.py" \
       --prefix PATH ":" ${
         lib.makeBinPath [
-          "/run/wrappers/bin"
           #coreutils      # cat, mkdir, rm, tail, tee
           #gnugrep        # grep
           lsof           # lsof
