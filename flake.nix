bddda0a8cadd35398d3b57c96c6ba6d6f8d2ae94
Jademalo <386846+Jademalo@users.noreply.github.com>
2026-09-08 11:07:36 +0000

    Re-add dev-shell and package

--- flake.nix
+++ flake.nix
@@ -5,19 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = 
-    { 
-      self, 
-      nixpkgs 
-    }:
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+    in
     {
+      packages = forAllSystems (system: rec {
+        webzfs = (pkgsFor system).callPackage ./ports/nix/package.nix { };
+        default = webzfs;
+      });
+
+      devShells = forAllSystems (system: {
+        default = import ./ports/nix/dev-shell.nix { pkgs = pkgsFor system; };
+      });
 
       nixosModules = rec {
         webzfs = import ./ports/nix/module.nix;
-        default = { ... }: {
-          imports = [ webzfs ];
-        };
+        default = webzfs;
       };
-
     };
 }
